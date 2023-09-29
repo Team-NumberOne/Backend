f0dcc93a2f95f4b3b598f669cbd75cbd5854e384
versatile0010 <siam0294@naver.com>
2023-09-30 02:18:45 +0900

    Chore(#2): ci-cd setting

---
+++ src/main/java/com/numberone/backend/HelloController.java
@@ -0,0 +1,14 @@
+package com.numberone.backend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        log.info("[hello controller is called]");
+        return "Hello, NumberOne";
+    }
+}
