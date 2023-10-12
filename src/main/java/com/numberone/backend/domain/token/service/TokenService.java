5a4f2562d289da7137c375bbcb7574b9835e5053
nohy6630 <nohy6630@gmail.com>
2023-10-12 18:49:09 +0900

    Feat(#10): 네이버 카카오 인가코드 이용해 사용자 정보 추출

--- src/main/java/com/numberone/backend/domain/token/service/TokenService.java
+++ src/main/java/com/numberone/backend/domain/token/service/TokenService.java
@@ -1,16 +1,12 @@
 package com.numberone.backend.domain.token.service;
 
 import com.numberone.backend.domain.token.dto.request.TokenRequest;
-import com.numberone.backend.domain.token.dto.response.KakaoInfoResponse;
-import com.numberone.backend.domain.token.dto.response.KakaoTokenResponse;
-import com.numberone.backend.domain.token.dto.response.TokenResponse;
+import com.numberone.backend.domain.token.dto.response.*;
 import com.numberone.backend.properties.KakaoProperties;
+import com.numberone.backend.properties.NaverProperties;
 import com.numberone.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -22,6 +18,7 @@ public class TokenService {
     private final JwtUtil jwtUtil;
     private final RestTemplate restTemplate;
     private final KakaoProperties kakaoProperties;
+    private final NaverProperties naverProperties;
 
     public TokenResponse loginKakao(TokenRequest tokenRequest) {
         HttpHeaders headers = new HttpHeaders();
@@ -50,7 +47,35 @@ public class TokenService {
         return new TokenResponse(token);
     }
 
-    public TokenResponse loginNaver(TokenRequest tokenRequest){
-        return null;
+    public TokenResponse loginNaver(TokenRequest tokenRequest) {
+        System.out.println(tokenRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        NaverTokenResponse naverResponse = restTemplate.postForObject(
+                "https://nid.naver.com/oauth2.0/token?grant_type={grant_type}&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}",
+                new HttpEntity<>(null, headers),
+                NaverTokenResponse.class,
+                "authorization_code",
+                naverProperties.getClient_id(),
+                naverProperties.getClient_secret(),
+                tokenRequest.getCode(),
+                "kusitms"
+        );
+
+        System.out.println(naverResponse);
+        System.out.println(naverResponse.getAccess_token());
+
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization", "Bearer " + naverResponse.getAccess_token());
+
+        ResponseEntity<NaverInfoResponse> response = restTemplate.exchange("https://openapi.naver.com/v1/nid/me", HttpMethod.GET, new HttpEntity<>(null, headers), NaverInfoResponse.class);
+
+        System.out.println(response.getBody().getResponse().getEmail());
+
+        String token = jwtUtil.createToken(response.getBody().getResponse().getEmail(), 1000L * 60 * 60 * 24 * 14);
+
+        return new TokenResponse(token);
     }
 }
