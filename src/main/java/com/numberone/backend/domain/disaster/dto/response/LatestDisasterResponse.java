744f6bbf1c3b88ff76c3d1949df99b8afa509adf
nohy6630 <nohy6630@gmail.com>
2023-11-03 17:04:48 +0900

    Feat(#43): Swagger 설명 추가

--- src/main/java/com/numberone/backend/domain/disaster/dto/response/LatestDisasterResponse.java
+++ src/main/java/com/numberone/backend/domain/disaster/dto/response/LatestDisasterResponse.java
@@ -2,6 +2,7 @@ package com.numberone.backend.domain.disaster.dto.response;
 
 import com.numberone.backend.domain.disaster.entity.Disaster;
 import com.numberone.backend.domain.disaster.util.DisasterType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,10 +10,15 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class LatestDisasterResponse {
+    @Schema(defaultValue = "화재")
     private String disasterType;
+    @Schema(defaultValue = "2")
     private Integer severity;
+    @Schema(defaultValue = "서울특별시 강남구 동작동 화재 발생")
     private String title;
+    @Schema(defaultValue = "금일 10.23. 19:39경 소촌동 855 화재 발생, 인근주민은 안전유의 및 차량우회바랍니다. 960-8222")
     private String msg;
+    @Schema(defaultValue = "서울특별시 강남구 ・ 오후 2시 46분")
     private String info;
 
     public static LatestDisasterResponse of(Disaster disaster) {
