262a86ce8779a2bea225c16c65820e317af620a4
Jaehyeon <wogusn@gmail.com>
2023-11-17 00:25:32 +0900

    Feat(#54): 댓글 리스트 조회 시, 좋아요 여부 반환하도록 변경

--- src/main/java/com/numberone/backend/domain/comment/service/CommentService.java
+++ src/main/java/com/numberone/backend/domain/comment/service/CommentService.java
@@ -9,6 +9,8 @@ import com.numberone.backend.domain.comment.dto.response.CreateChildCommentRespo
 import com.numberone.backend.domain.comment.dto.response.GetCommentDto;
 import com.numberone.backend.domain.comment.entity.CommentEntity;
 import com.numberone.backend.domain.comment.repository.CommentRepository;
+import com.numberone.backend.domain.like.entity.CommentLike;
+import com.numberone.backend.domain.like.repository.CommentLikeRepository;
 import com.numberone.backend.domain.member.entity.Member;
 import com.numberone.backend.domain.member.repository.MemberRepository;
 import com.numberone.backend.domain.token.util.SecurityContextProvider;
@@ -32,6 +34,7 @@ public class CommentService {
     private final ArticleRepository articleRepository;
     private final ArticleParticipantRepository articleParticipantRepository;
     private final MemberRepository memberRepository;
+    private final CommentLikeRepository commentLikeRepository;
 
     @Transactional
     public CreateChildCommentResponse createChildComment(
@@ -56,10 +59,15 @@ public class CommentService {
     }
 
     public List<GetCommentDto> getCommentsByArticle(Long articleId) {
+        String principal = SecurityContextProvider.getAuthenticatedUserEmail();
+        Member member = memberRepository.findByEmail(principal)
+                .orElseThrow(NotFoundMemberException::new);
         Article article = articleRepository.findById(articleId)
                 .orElseThrow(NotFoundArticleException::new);
         List<GetCommentDto> comments = commentRepository.findAllByArticle(article.getId());
-
+        List<Long> likedCommentIdList = commentLikeRepository.findByMember(member)
+                .stream().map(CommentLike::getCommentId)
+                .toList();
         // 계층 구조로 변환 (추후 리팩토링 필요)
         List<GetCommentDto> result = new ArrayList<>();
         Map<Long, GetCommentDto> map = new HashMap<>();
@@ -68,7 +76,7 @@ public class CommentService {
                     CommentEntity commentEntity = commentRepository.findById(comment.getCommentId())
                             .orElseThrow(NotFoundCommentException::new);
                     Optional<Member> author = memberRepository.findById(commentEntity.getAuthorId());
-                    comment.updateCommentInfo(author);
+                    comment.updateCommentInfo(author, likedCommentIdList);
 
                     map.put(comment.getCommentId(), comment);
 
