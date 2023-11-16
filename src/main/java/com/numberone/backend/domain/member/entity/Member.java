ff8be055fe0b1e01d89e6e9e01c014c9ccf7b5c6
Jaehyeon <wogusn@gmail.com>
2023-11-16 23:14:20 +0900

    Feat(#54): fix invalid uri prefix

--- src/main/java/com/numberone/backend/domain/member/entity/Member.java
+++ src/main/java/com/numberone/backend/domain/member/entity/Member.java
@@ -26,7 +26,6 @@ import java.util.List;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@RequestMapping("/api/members")
 @Table(name = "MEMBER")
 public class Member extends BaseTimeEntity {
     @Id
