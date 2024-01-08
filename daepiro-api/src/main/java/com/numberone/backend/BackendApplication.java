9008e6dd641ec7b6dc92e659158353ee71f52745
Jaehyeon <wogusn@gmail.com>
2024-01-09 00:01:13 +0900

    Feat: set main class for jib

--- daepiro-api/src/main/java/com/numberone/backend/BackendApplication.java
+++ daepiro-api/src/main/java/com/numberone/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.numberone.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
