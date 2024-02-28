6790271585ddb8649a08fd4c1d86d34ed0d49ab6
nohy6630 <nohy6630@gmail.com>
2024-02-28 16:41:17 +0900

    Feat: Open Feign 적용

--- daepiro-app/src/main/java/com/numberone/backend/BackendApplication.java
+++ daepiro-app/src/main/java/com/numberone/backend/BackendApplication.java
@@ -1,14 +1,19 @@
 package com.numberone.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})//https://stackoverflow.com/questions/74593433/consider-defining-a-bean-of-type-org-springframework-cloud-openfeign-feignconte
 public class BackendApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
