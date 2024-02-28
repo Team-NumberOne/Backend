b2127b01e6a98e7b88f47c4192b859328394b8d6
nohy6630 <nohy6630@gmail.com>
2024-02-28 16:37:57 +0900

    Chore: 필요없는 파일 삭제

--- daepiro-common/src/main/java/com/numberone/backend/exception/badrequest/BadRequestSocialTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.numberone.backend.exception.badrequest;
-
-import static com.numberone.backend.exception.context.CustomExceptionContext.BAD_REQUEST_SOCIAL_TOKEN;
-
-public class BadRequestSocialTokenException extends BadRequestException{
-    public BadRequestSocialTokenException() {
-        super(BAD_REQUEST_SOCIAL_TOKEN);
-    }
-}
--- daepiro-common/src/main/java/com/numberone/backend/exception/notfound/NotFoundRefreshTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.numberone.backend.exception.notfound;
-
-import static com.numberone.backend.exception.context.CustomExceptionContext.WRONG_REFRESH_TOKEN;
-
-public class NotFoundRefreshTokenException extends NotFoundException {
-    public NotFoundRefreshTokenException() {
-        super(WRONG_REFRESH_TOKEN);
-    }
-}
