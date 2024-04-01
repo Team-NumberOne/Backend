099abd1c1c914ccca796bc7ba40f0da50c13c971
nohy6630 <nohy6630@gmail.com>
2024-04-01 21:12:11 +0900

    Rename: 카카오 로그인 네이밍 수정

--- daepiro-auth/src/main/java/com/numberone/backend/provider/KakaoAuthenticationProvider.java
+++ daepiro-auth/src/main/java/com/numberone/backend/provider/KakaoAuthenticationProvider.java
@@ -1,7 +1,5 @@
 package com.numberone.backend.provider;
 
-import com.numberone.backend.config.AspectConfig;
-import com.numberone.backend.exception.notfound.NotFoundMemberException;
 import com.numberone.backend.domain.member.entity.Member;
 import com.numberone.backend.domain.member.repository.MemberRepository;
 import com.numberone.backend.feign.KakaoFeign;
@@ -11,28 +9,27 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class SocialAuthenticationProvider implements AuthenticationProvider {
+public class KakaoAuthenticationProvider implements AuthenticationProvider {
     private final KakaoFeign kakaoFeign;
     private final MemberRepository memberRepository;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String token = (String) authentication.getPrincipal();
-        Long socialId;
+        Long kakaoId;
         try {
-            socialId = kakaoFeign.getUserData(JwtProvider.PREFIX_BEARER + token).getId();
+            kakaoId = kakaoFeign.getUserData(JwtProvider.PREFIX_BEARER + token).getId();
         } catch (Exception e) {
             throw new BadCredentialsException("유효하지 않은 OAuth 토큰입니다.");
         }
-        Member member = memberRepository.findBySocialId(socialId)
+        Member member = memberRepository.findByKakaoId(kakaoId)
                 .orElse(null);
         if (member == null)
-            member = memberRepository.save(Member.of(socialId));
+            member = memberRepository.save(Member.ofKakao(kakaoId));
         return UsernamePasswordAuthenticationToken.authenticated(member.getId(), null, null);
     }
 
